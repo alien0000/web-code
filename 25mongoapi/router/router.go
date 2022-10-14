44951c68bdcb5e97e7a4a9d7e15c5a25767d2ce0
sajdakabir <sajda.kbir.com>
2022-10-14 22:26:46 +0530

    added all the router

--- 25mongoapi/router/router.go
+++ 25mongoapi/router/router.go
@@ -11,6 +11,7 @@ func Router() *mux.Router {
 
 	router.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
 	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+
 	router.HandleFunc("/api/movie/{id}", controller.MarkAsWatch).Methods("PUT")
 	router.HandleFunc("/api/movie/{id}", controller.DeleteAMovie).Methods("DELETE")
 	router.HandleFunc("/api/deleteallmovie", controller.DeleteAllMovie).Methods("DELETE")
