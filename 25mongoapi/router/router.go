6b0a40ec162c12b60f8c0f32abfc338f39ab573e
Sajda Kabir <sajda.kbir@gmail.com>
2023-10-15 11:20:17 +0530

    Added a route for add movie List

--- 25mongoapi/router/router.go
+++ 25mongoapi/router/router.go
@@ -12,6 +12,7 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
 	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
 	router.HandleFunc("/api/movie/{id}", controller.MarkAsWatch).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}", controller.AddMovie).Methods("POST")
 	router.HandleFunc("/api/movie/{id}", controller.DeleteAMovie).Methods("DELETE")
 	router.HandleFunc("/api/deleteallmovie", controller.DeleteAllMovie).Methods("DELETE")
 	return router
